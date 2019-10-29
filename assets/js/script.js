33fcd0c60804e42ba7447352b65376c7271dd60e
jenny80170 <jennifer.malleret@outlook.fr>
2019-10-29 15:43:33 +0100

    changer la couleur du texte en cliquant sur les boutons

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Fonction qui récupére l'ID et qui attribut un style color rouge
+function colorRed(){
+  document.getElementById('text').style.color = 'red';
+}
+document.querySelector('.red').onclick = colorRed;
+// Fonction qui récupére l'ID et qui attribut un style color vert
+function colorGreen(){
+  document.getElementById('text').style.color = 'green';
+}
+document.querySelector('.green').onclick = colorGreen;
+// Fonction qui récupére l'ID et qui attribut un style color blue
+function colorBlue(){
+  document.getElementById('text').style.color = 'blue';
+}
+document.querySelector('.blue').onclick = colorBlue;
